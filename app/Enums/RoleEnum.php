fe9ab10e681e94588766e4f3fc68e3f0e13ac19b
Raphael Heying <raphael.h@hotmail.com>
2024-09-26 01:19:29 -0300

    remove unused function

--- app/Enums/RoleEnum.php
+++ app/Enums/RoleEnum.php
@@ -6,9 +6,4 @@ enum RoleEnum: string
 {
     case Admin = 'admin';
     case User = 'user';
-
-    public static function getValues(): array
-    {
-        return array_column(self::cases(), 'value');
-    }
 }
